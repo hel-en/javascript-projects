380966a98ed1c6a90ed64fd71659faf3dbdb81f4
User <helen@MacBook-Pro-53712.local>
2024-04-10 18:56:19 -0500

    Practiced newline and tabs

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,6 @@
-console.log('Hello, JavaScript.');
-console.log(2001);
-console.log("What","do","commas","do?");
-console.log("Does", "adding",      "space", "matter?");
-console.log('Launch' + 'Code');
-console.log("LaunchCode was founded in", 2013);
+console.log('Hello, JavaScript.'); // regular
+console.log(2001); // no quotation marks--is integer
+console.log("What","do","commas","do?"); // commas add a single space between quoted words
+console.log("Does", "adding",      "space", "matter?"); // space in code does not matter; single space is still printed
+console.log('Launch' + 'Code'); // prints words without spaces between
+console.log("LaunchCode was founded in", 2013); // text plus integer, separated by comma
--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,17 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+
+// Practice with \n and \t
+
+console.log('Hello JavaScript.');
+console.log("LaunchCode", 2024); 
+console.log("Unit 1\nUnit 2\nUnit 3\nUnit 4"); // pushes each word to new line
+console.log("Does\tadding      space\tmatter\tif \t you\t use\tthe tab command?"); // seemingly yes
+console.log("Does\tadding space\tmatter\tif\tyou\tuse\tthe tab command?");
+console.log("Does\tadding\t\tspace\tmatter\tif\tyou\tuse\tthe\ttab\tcommand?"); // adding multiple forces multiple tabs
+console.log('Launch' + 'Code'); 
+console.log("LaunchCode was founded in", 2013); 
+console.log("Unit 1\n\tLearn\n\t\tHow\n\t\t\tto\n\t\t\t\tCode");
+console.log("Unit 1\n\tLearn\n\tHow\n\tto\n\tCode\nUnit 2\n\tNext\n\tSteps");
