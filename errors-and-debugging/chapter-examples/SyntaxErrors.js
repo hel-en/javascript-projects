2f3b332f572c21e2dacba4dec774fc7aad8195ab
hel-en <github.slashing152@passinbox.com>
2024-04-30 23:01:03 -0500

    completed chapter 6 examples and exercises

--- errors-and-debugging/chapter-examples/SyntaxErrors.js
+++ errors-and-debugging/chapter-examples/SyntaxErrors.js
@@ -1,2 +1,14 @@
-let day = Wednesday;
-console.log(day;
+/*
+let day = Wednesday; 
+needs quotation marks
+*/
+// let day = "Wednesday";
+
+/*
+console.log(day; 
+needs closing parentheses
+*/
+//console.log(day);
+
+let firstName = "Jack";
+console.log(firstname);
--- errors-and-debugging/chapter-examples/degrees-c-to-k.js
+++ errors-and-debugging/chapter-examples/degrees-c-to-k.js
@@ -1,6 +1,15 @@
 const input = require('readline-sync');
 
 let degreesC = input.question('Temp in degrees C: ');
+// degreesC = Number(degreesC);
 let degreesK = degreesC + 273.15;
+// console.log(degreesC);
+// console.log(typeof degreesC);
 
 console.log('Degrees K:', degreesK);
+
+
+/*
+must invesigate variable, then determine typeof, then convert it to the correct data type
+correct code is shown as comments
+*/
